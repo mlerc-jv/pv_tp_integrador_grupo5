6fb9a8dc915cf2665e3459e07e369d29d3e9b374
mlerc-jv <jimenavirmeru@gmail.com>
2026-06-21 21:31:34 -0300

    Agrego footer.css - Meruvia Jimena

--- src/pages/ListaClientes.jsx
+++ src/pages/ListaClientes.jsx
@@ -1,118 +1,125 @@
-import '../css/listaclientes.css'
+import "../css/listaclientes.css"
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FormCliente from "../components/FormCliente";
 
 const ListaClientes = () => {
-const [clientes, setClientes] = useState([]);
-const [busqueda, setBusqueda] = useState("");
-const [loading, setLoading] = useState(true);
-const [error, setError] = useState(false);
+  const [clientes, setClientes] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-useEffect(() => {
-  fetch("https://fakestoreapi.com/users")
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error("Error al obtener clientes");
-    }
-    return res.json();
-  })
-  .then((data) => {
-    setClientes(data);
-    setLoading(false);
-  })
-  .catch(() => {
-    setError(true);
-    setLoading(false);
-  });
-}, []);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/users")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Error al obtener clientes");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setClientes(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
 
-const clientesFiltrados = clientes.filter(
-  (cliente) =>
-    cliente.name.lastname
-  .toLowerCase()
-  .includes(busqueda.toLowerCase()) ||
-  cliente.address.city
-  .toLowerCase()
-  .includes(busqueda.toLowerCase())
-);
+  const clientesFiltrados = clientes.filter(
+    (cliente) =>
+      cliente.name.lastname
+        .toLowerCase()
+        .includes(busqueda.toLowerCase()) ||
+      cliente.address.city
+        .toLowerCase()
+        .includes(busqueda.toLowerCase())
+  );
 
-if (loading) {
-  return <h2>Cargando clientes...</h2>;
-}
+  if (loading) {
+    return <h2>Cargando clientes...</h2>;
+  }
 
-if (error) {
-  return <h2>Error al cargar los clientes.</h2>;
-}
+  if (error) {
+    return <h2>Error al cargar los clientes.</h2>;
+  }
 
-return ( 
-<div className="clientes-container">
-  <h1>Clientes</h1>
-  <FormCliente />
-  <hr />
+  return (
+    <div className="clientes-container">
 
-  <div className="contenedor-buscador">
-    <h2 className="titulo-buscador">Buscar Clientes</h2>
+      <h1>Clientes</h1>
+      <FormCliente />
 
-    <input
-      className="buscador"
-      type="text"
-      placeholder="Buscar por apellido o ciudad"
-      value={busqueda}
-      onChange={(e) => setBusqueda(e.target.value)}
-    />
+      <hr />
 
-    <p className="cantidad-clientes">
-      Clientes encontrados: {clientesFiltrados.length}
-    </p>
+      <div className="contenedor-buscador">
 
-  </div>
+        <h2 className="titulo-buscador">
+          Buscar Clientes
+        </h2>
 
-  <table className="tabla-clientes">
+        <input
+          className="buscador"
+          type="text"
+          placeholder="Buscar por apellido o ciudad"
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+        />
 
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Email</th>
-        <th>Teléfono</th>
-        <th>Ciudad</th>
-        <th>Acciones</th>
-      </tr>
-    </thead>
+        <p className="cantidad-clientes">
+          Clientes encontrados: {clientesFiltrados.length}
+        </p>
 
-    <tbody>
-      {clientesFiltrados.map((cliente) => (
-        <tr key={cliente.id}>
-          <td>{cliente.id}</td>
+      </div>
+      <table className="tabla-clientes">
 
-          <td>
-            {cliente.name.firstname} {cliente.name.lastname}
-          </td>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Ciudad</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
 
-          <td>{cliente.email}</td>
+        <tbody>
 
-          <td>{cliente.phone}</td>
+          {clientesFiltrados.map((cliente) => (
+            <tr key={cliente.id}>
 
-          <td>{cliente.address.city}</td>
+              <td>{cliente.id}</td>
 
-          <td>
-            <Link
-              className="btn-ficha"
-              to={`/clientes/${cliente.id}`}
-            >
-              Ver Ficha Completa
-            </Link>
-          </td>
-        </tr>
-      ))}
-    </tbody>
+              <td>
+                {cliente.name.firstname} {cliente.name.lastname}
+              </td>
 
-  </table>
+              <td>{cliente.email}</td>
 
-</div>
+              <td>{cliente.phone}</td>
 
-);
+              <td>{cliente.address.city}</td>
+
+              <td>
+                <Link
+                  className="btn-ficha"
+                  to={`/clientes/${cliente.id}`}
+                >
+                  Ver Ficha Completa
+                </Link>
+              </td>
+
+            </tr>
+          ))}
+
+        </tbody>
+
+      </table>
+
+    </div>
+  );
 };
 
 export default ListaClientes;
